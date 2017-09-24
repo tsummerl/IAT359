dae7ce3f8d4b7b94a4b4c4b84385a8eea87b31b7
T <twisummerland@gmail.com>
2017-09-23 21:54:48 -0700

    Implemented the functionality for updating final mark

--- Lab3/app/src/main/java/summerland/twilight/lab3/MainActivity.java
+++ Lab3/app/src/main/java/summerland/twilight/lab3/MainActivity.java
@@ -2,9 +2,15 @@ package summerland.twilight.lab3;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.View;
 import android.widget.EditText;
+import android.widget.SeekBar;
+import android.widget.Button;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements TextWatcher, View.OnClickListener {
 
     private static final String DEBUG_TAG = "CourseGrades";
     private static final String ASSIGNMENTS = "ASSIGNMENTS";
@@ -27,30 +33,152 @@ public class MainActivity extends AppCompatActivity {
 
     private double exam;
     private EditText examEditText;
+    private SeekBar examSeek;
 
+    private TextView finalMark;
+
+    private Button resetButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        assgnEditText.findViewById()
-        // agnEditTesxt.addtextChangedListener(this);
-    }
-    @Override
-    public void onTextChanged(CharSequence s, int start, int end, int i2)
-    {
-        ;
-//        // if(assgnEditText.isFocused())
-//        {
-//            try
-//            {
-//
-//            }
-//            catch (NumberFormatException e)
-//            {
-//                assgnScore = 0.0
-//            }
-//        }
+        assgnEditText = (EditText)findViewById(R.id.assignmentEdit);
+        partEditText = (EditText)findViewById(R.id.participationEdit);
+        projectEditText = (EditText)findViewById(R.id.projectEdit);
+        quizzesEditText = (EditText)findViewById(R.id.quizzesEdit);
+        examEditText= (EditText) findViewById(R.id.examEdit);
+        examSeek = (SeekBar) findViewById(R.id.examSeekBar);
+        resetButton = (Button) findViewById(R.id.resetButton);
+        finalMark = (TextView) findViewById(R.id.finalTextView);
+
+        assgnEditText.addTextChangedListener(this);
+        partEditText.addTextChangedListener(this);
+        projectEditText.addTextChangedListener(this);
+        quizzesEditText.addTextChangedListener(this);
+        examEditText.addTextChangedListener(this);
+
+        examSeek.setOnSeekBarChangeListener(examSeekBarListener);
+//        resetButton.setOnClickListener(this);
     }
 
+    private void updateStandard()
+    {
+        double finalMark = assgn*15/100 + participation*15/100 + project*20/100 + quizzes*20/100 + exam*30/100;
+        this.finalMark.setText(String.format("%.02f", finalMark));
+    }
+
+    private void updateExam()
+    {
+        examEditText.setText(String.format("%d", (int)exam));
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState)
+    {
+        super.onSaveInstanceState(outState);
+        outState.putDouble(ASSIGNMENTS, assgn);
+        outState.putDouble(PROJECT, project);
+        outState.putDouble(QUIZZES, quizzes);
+        outState.putDouble(PARTICIPATE, participation);
+        outState.putDouble(EXAM, exam);
+    }
+
+    @Override
+    protected void onRestoreInstanceState(Bundle inState)
+    {
+        super.onRestoreInstanceState(inState);
+        assgn = inState.getDouble(ASSIGNMENTS);
+        project = inState.getDouble(PROJECT);
+        quizzes = inState.getDouble(QUIZZES);
+        participation = inState.getDouble(PARTICIPATE);
+        exam = inState.getDouble(EXAM);
+    }
+    @Override
+    public void onTextChanged(CharSequence s, int start, int before, int count)
+    {
+        if (assgnEditText.isFocused())
+        {
+            try
+            {
+                assgn = Double.parseDouble(s.toString());
+            }
+            catch (NumberFormatException e)
+            {
+                assgn = 0.0;
+            }
+        }
+        else if (partEditText.isFocused())
+        {
+            try
+            {
+                participation = Double.parseDouble(s.toString());
+            }
+            catch (NumberFormatException e)
+            {
+                participation = 0.0;
+            }
+        }
+        else if (projectEditText.isFocused())
+        {
+            try
+            {
+                project = Double.parseDouble(s.toString());
+            }
+            catch (NumberFormatException e)
+            {
+                project = 0.0;
+            }
+        }
+        else if (quizzesEditText.isFocused())
+        {
+            try
+            {
+                quizzes = Double.parseDouble(s.toString());
+            }
+            catch (NumberFormatException e)
+            {
+                quizzes = 0.0;
+            }
+        }
+        updateStandard();
+    }
+
+    @Override
+    public void beforeTextChanged(CharSequence s, int start, int count, int after)
+    {
+
+    }
+
+    @Override
+    public void afterTextChanged(Editable s)
+    {
+
+    }
+
+    @Override
+    public void onClick(View v)
+    {
+
+    }
+
+    private SeekBar.OnSeekBarChangeListener examSeekBarListener = new SeekBar.OnSeekBarChangeListener() {
+        @Override
+        public void onProgressChanged(SeekBar seekBar, int i, boolean b)
+        {
+            exam = seekBar.getProgress();
+            updateExam();
+            updateStandard();
+        }
+
+        @Override
+        public void onStartTrackingTouch(SeekBar seekBar) {
+
+        }
+
+        @Override
+        public void onStopTrackingTouch(SeekBar seekBar) {
+
+        }
+    };
 }
